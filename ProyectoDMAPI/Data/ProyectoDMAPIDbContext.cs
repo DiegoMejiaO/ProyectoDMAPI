f5abcd1618346cfbee9cb1f94b497bac8081e0c5
Diego Mauricio Mejía Ospina <77675522+DiegoMejiaO@users.noreply.github.com>
2023-07-24 14:18:38 -0500

    Initial commit

---
+++ ProyectoDMAPI/Data/ProyectoDMAPIDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProyectoDMAPI.Data.Models;
+
+namespace ProyectoDMAPI.Data
+{
+    public class ProyectoDMAPIDbContext : DbContext
+    {
+        public ProyectoDMAPIDbContext (DbContextOptions<ProyectoDMAPIDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProyectoDMAPI.Data.Models.Client> Client { get; set; } = default!;
+    }
+}
---
+++ ProyectoDMAPI/Migrations/20230722161506_SeedDb.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProyectoDMAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Dna",
+                table: "Client",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Dna",
+                table: "Client");
+        }
+    }
+}
